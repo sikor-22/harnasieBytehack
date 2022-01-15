bad21e4fe522aee854171576f687bc1621ad1ff2
Matuszvv <wiktor.mx@gmail.com>
2022-01-15 14:37:12 +0100

    branched

--- main.py
+++ main.py
@@ -20,10 +20,58 @@ print(df.head())
 print(df.info())
 
 sns.countplot(x = 'target',data = df)
-plt.show()
+# plt.show()
 
 df.drop(['date','flag','username'], axis=1, inplace=True)
 df.drop('id', axis=1, inplace=True)
 
 print(df.head())
 
+
+# FOR STOP WORDS
+import nltk
+nltk.download('omw-1.4')
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+
+print('\n\nDownloaded packages\n\n')
+from nltk.corpus import stopwords
+stop_words = stopwords.words('english')
+
+# FOR LEMMATIZATION
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+
+# FOR STEMMING
+from nltk.stem import PorterStemmer
+stemmer = PorterStemmer()
+
+def data_preprocessing(raw_text):
+    #Data Cleansing
+    sentence = re.sub(r'[^\w\s]', ' ',raw_text )
+    #Removing numbers
+    sentence = re.sub(r'[0-9]', '', sentence)
+    #Tokenization
+    words = nltk.word_tokenize(sentence)
+    #Lowercase
+    for word in words:
+            word.lower()
+    #Stop words removal
+    words = [w for w in words if not w in stop_words]
+    #stemming
+    words = [stemmer.stem(w) for w in words]
+    #Lemmatization
+    final_words = [lemmatizer.lemmatize(w) for w in words]
+    return  final_words 
+
+# FOR STEMMING
+# from nltk.stem import PorterStemmer
+# stemmer = PorterStemmer()
+
+
+# df.tweet = df.tweet.apply(data_preprocessing)
+
+# df.tweet = df.tweet.apply(data_preprocessing)
+
+print(df.tweet)
